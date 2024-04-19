89e2f89fdc5c5fbddcc1aea9d1a0c73a343df147
Harsh-Pandey <harshcse7571@gmail.com>
2024-04-19 10:39:12 -0700

    variables in Javascript

---
+++ Topic2_Basics/variables_In_Js.js
@@ -0,0 +1,42 @@
+// vairables are used to store information 
+
+var name = "Harsh"
+
+// The variables which are created by using var keyword are function scoped where as const and let are blocked scope
+
+// {} :- Block
+
+function sayName(){
+    var lName = "Pandey"
+}
+sayName()
+// console.log(lname);          ------------> variable is not defined 
+
+
+// lets check for let and const
+let age = 15 
+const dob = "1/1/1"
+
+function details(){
+    console.log(`name is ${name} and age is ${age} and dob is ${dob}`);
+}
+details()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
